f0f72f99d6cff70cc3d73cf6e8a0aa0eb1874172
shifasarthar <111578226+shifasarthar@users.noreply.github.com>
2022-11-17 21:00:37 +0530

    code of the day

---
+++ ReverseAStringcode1.java
@@ -0,0 +1,37 @@
+package codeoftheday;
+
+public class ReverseAStringcode1 {
+
+	public static void main(String args[]) {
+		ReverseAStringcode1 obj=new ReverseAStringcode1();
+		obj.reverse1();
+		obj.reverse2();
+		System.out.println(obj.reverse3(obj.str));
+		obj.reverse4();
+	}
+	String str="Golden Touch july 2022";
+	public void reverse1() {
+		char[]strArray=str.toCharArray();
+		for(int i=strArray.length-1;i>=0;i--)
+			{
+			System.out.print(strArray[i]);
+			}
+		}
+	public void reverse2() {
+		for(int i=str.length()-1;i>=0;i--) {
+		System.out.print(str.substring(i,i+1));	
+		}
+	}
+	public String reverse3(String str) {
+		if((null==str)||(str.length()<=1))
+		{
+			return str;
+		}
+		return reverse3(str.substring(1)+str.charAt(0));
+	}
+
+public void reverse4() {
+	StringBuffer sbf=new StringBuffer(str);
+	System.out.println(sbf.reverse());
+}
+}
